60437cf3f6aa2dac78fa7eb4972ac4ac364545db
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2022-04-13 14:23:01 +0300

    update

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,29 +1,33 @@
-const createError = require('http-errors')
+const createError = require('http-errors');
 const { MESSAGES, REQ_VALIDATION_TARGET } = require('../utils/constants');
 
 const validation = (schema, validationTarget) => {
   return async (req, res, next) => {
-    let validated = null
+    let validated = null;
 
     switch (validationTarget) {
       case REQ_VALIDATION_TARGET.QUERY:
-        validated = schema.validate(req.query)
-        break
+        validated = schema.validate(req.query);
+        break;
       case REQ_VALIDATION_TARGET.PARAMS:
-        validated = schema.validate(req.params)
-        break
+        validated = schema.validate(req.params);
+        break;
       case REQ_VALIDATION_TARGET.BODY:
-        validated = schema.validate(req.body)
-        break
+        validated = schema.validate(req.body);
+
+        if (!validated.error) {
+          req.body = validated.value;
+        }
+
+        break;
       default:
-        throw createError(500, MESSAGES.SERVER_ERROR)
+        throw createError(500, MESSAGES.SERVER_ERROR);
     }
 
-    if (validated.error) throw createError(400, validated.error.message)
+    if (validated.error) throw createError(400, validated.error.message);
 
-    req.body = validated.value
-    next()
-  }
-}
+    next();
+  };
+};
 
-module.exports = validation
+module.exports = validation;

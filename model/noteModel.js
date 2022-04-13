57818e90c5313430ccde26ddd2186286cbec2cbc
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2022-04-13 17:16:38 +0300

    added mongoDB model for notes

---
+++ model/noteModel.js
@@ -0,0 +1,58 @@
+const { Schema, model } = require('mongoose');
+const path = require('path');
+const { CATEGORIES } =require('../utils/constants');
+const getIsoDateInterval = require('../utils/getDatesInterval');
+
+const notesSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'category is required']
+    },
+    category: {
+        type: String,
+        required: [true, 'category is required']
+    },
+    content: {
+        type: String,
+        required: [true, 'category is required']
+    },
+    dates: {
+        type: Array,
+        required: [true, 'dates is required']
+    },
+    icon: {
+        type: String
+    }
+}, { versionKey: false, timestamps: true });
+
+notesSchema.pre('save', function (next) {
+    if (this.isNew) {
+        let currentIcon = null;
+
+        switch (this.category) {
+            case CATEGORIES.TASK:
+                currentIcon = 'local_grocery_store.svg';
+                break;
+            case CATEGORIES.RANDOM_THOUGHT:
+                currentIcon = 'psychology.svg';
+                break;
+            case CATEGORIES.QUOTE:
+                currentIcon = 'format_quote.svg';
+                break;
+            case CATEGORIES.IDEA:
+                currentIcon = 'lightbulb_outline.svg';
+                break;
+            default:
+                currentIcon = 'default.svg';
+        }
+        
+        this.icon = path.join('icons', currentIcon);
+    }
+
+    this.dates = getIsoDateInterval(this.content);
+    next();
+});
+
+const Note = model('note', notesSchema);
+
+module.exports = Note;

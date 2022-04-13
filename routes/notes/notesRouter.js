0e8e68130f97960c77f8717eb80020ab26fd8bdb
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2022-04-13 20:23:00 +0300

    update

--- routes/notes/notesRouter.js
+++ routes/notes/notesRouter.js
@@ -1,19 +1,38 @@
 const express = require('express');
-const ctrl = require('../../controllers');
+const { addNoteController, updateNoteController } = require('../../controllers');
 const validation = require('../../middlewares/validation');
 const wrapper = require('../../utils/wrapper');
-const { noteDataSchema } = require('../../utils/validationSchemas');
 const { REQ_VALIDATION_TARGET } = require('../../utils/constants');
+const {
+    addNoteDataSchema,
+    noteIdSchema,
+    updateNoteReqDataSchema
+} = require('../../utils/validationSchemas');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
-const validationMiddleware = validation(noteDataSchema, REQ_VALIDATION_TARGET.BODY);
+const addNoteBodyValidationMiddleware = validation(addNoteDataSchema, REQ_VALIDATION_TARGET.BODY);
+const paramsValidationMiddleware = validation(noteIdSchema, REQ_VALIDATION_TARGET.PARAMS);
+const updateNoteBodyValidationMiddleware = validation(updateNoteReqDataSchema, REQ_VALIDATION_TARGET.BODY);
 
-router.get('/stats', wrapper(ctrl.getNotesStatistics));
-router.get('/', wrapper(ctrl.getNotesListController));
-router.post('/',wrapper(validationMiddleware), wrapper(ctrl.addNoteController));
-router.get('/:id',wrapper(ctrl.getNoteController));
-router.delete('/:id', wrapper(ctrl.removeNoteController));
-router.patch('/:id', wrapper(ctrl.updateNoteController));
+// router.get('/stats', wrapper(ctrl.getNotesStatistics));
+
+// router.get('/', wrapper(ctrl.getNotesListController));
+
+router.post('/',
+    wrapper(addNoteBodyValidationMiddleware),
+    wrapper(addNoteController)
+);
+
+// router.get('/:id',wrapper(paramsValidationMiddleware), wrapper(ctrl.getNoteController));
+
+// router.delete('/:id', wrapper(paramsValidationMiddleware), wrapper(ctrl.removeNoteController));
+
+router.patch(
+    '/:id',
+    wrapper(paramsValidationMiddleware),
+    wrapper(updateNoteBodyValidationMiddleware),
+    wrapper(updateNoteController)
+);
 
 module.exports = router;

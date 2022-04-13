bafe31474a92de9f9b2b1d44f618953915720ca9
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2022-04-13 12:58:47 +0300

    added mongoDB connection and change errors variable name

--- bin/server.js
+++ bin/server.js
@@ -1,15 +1,22 @@
 require('colors');
-const { SUCCESS_MESSAGE } = require('../utils/messages');
+const mongoose = require('mongoose');
+const { MESSAGES } = require('../utils/constants');
 
 const app = require('../app');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, DB_HOST } = process.env;
 
-try {
-  app.listen(PORT, () => {
-    console.log(`${SUCCESS_MESSAGE.serverStartedSuccess}: ${PORT.cyan}`.green);
+mongoose
+  .connect(DB_HOST, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log(MESSAGES.DB_CONNECTION_SUCCESS.green);
+
+    app.listen(PORT, () => {
+      console.log(`${MESSAGES.SERV_CONNECTION_SUCCESS} on port ${PORT.cyan}`.green)
+    });
   })
-} catch (err) { 
-  console.log(err.message);
-  process.exit(1);
-}
+  .catch(err => {
+    console.log(MESSAGES.DB_CONNECTION_ERROR.red);
+    console.log(err.message);
+    process.exit(1);
+  });

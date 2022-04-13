b5fdc554e0912391b3266b2fa0a278b2a636f9d1
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2022-04-13 14:26:00 +0300

    update

--- routes/notes/notesRouter.js
+++ routes/notes/notesRouter.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const ctrl = require('../../controllers');
-const wrapper = require('../../utils/wrapper'); 
+const validation = require('../../middlewares/validation');
+const wrapper = require('../../utils/wrapper');
+const { noteDataSchema } = require('../../utils/validationSchemas');
+const { REQ_VALIDATION_TARGET } = require('../../utils/constants');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
+const validationMiddleware = validation(noteDataSchema, REQ_VALIDATION_TARGET.BODY);
 
 router.get('/stats', wrapper(ctrl.getNotesStatistics));
 router.get('/', wrapper(ctrl.getNotesListController));
-router.post('/', wrapper(ctrl.addNoteController));
+router.post('/',wrapper(validationMiddleware), wrapper(ctrl.addNoteController));
 router.get('/:id',wrapper(ctrl.getNoteController));
 router.delete('/:id', wrapper(ctrl.removeNoteController));
 router.patch('/:id', wrapper(ctrl.updateNoteController));
-
 
 module.exports = router;

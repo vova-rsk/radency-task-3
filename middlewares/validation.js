97fd393a77a86b1582ec6f064bc8e7ea6887dbd4
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2022-04-13 12:59:10 +0300

    added validation middleware

---
+++ middlewares/validation.js
@@ -0,0 +1,29 @@
+const createError = require('http-errors')
+const { MESSAGES, REQ_VALIDATION_TARGET } = require('../utils/constants');
+
+const validation = (schema, validationTarget) => {
+  return async (req, res, next) => {
+    let validated = null
+
+    switch (validationTarget) {
+      case REQ_VALIDATION_TARGET.QUERY:
+        validated = schema.validate(req.query)
+        break
+      case REQ_VALIDATION_TARGET.PARAMS:
+        validated = schema.validate(req.params)
+        break
+      case REQ_VALIDATION_TARGET.BODY:
+        validated = schema.validate(req.body)
+        break
+      default:
+        throw createError(500, MESSAGES.SERVER_ERROR)
+    }
+
+    if (validated.error) throw createError(400, validated.error.message)
+
+    req.body = validated.value
+    next()
+  }
+}
+
+module.exports = validation

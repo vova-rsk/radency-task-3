995cec81c5c8735a9d86bd90370a8fab9df551c6
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2022-04-13 17:16:13 +0300

    added static route for icons

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 const notesRouter = require('./routes/notes');
 const { MESSAGES } = require('./utils/constants');
 
 const app = express();
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+const iconsDir = path.join(__dirname, 'public', 'icons');
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
+app.use('/icons', express.static(iconsDir));
 app.use('/notes', notesRouter);
 
 app.use((req, res) => {

a4276adf3c5d23700b0ff6610b8f5212322e04f6
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2022-04-15 17:56:33 +0300

    removed unusefull code

--- services/controllers/addNoteController.ts
+++ services/controllers/addNoteController.ts
@@ -3,8 +3,7 @@ import repositories from '../../repositories';
 
 const addNote = async (req:Request, res:Response) => { 
     const urlHost = String(req.headers.host);
-    console.log('HEADERS ',req.headers);
-    console.log('SOCKET', req.socket);
+
     const result = await repositories.addNote(urlHost, req.body);
 
     res.status(201).json({

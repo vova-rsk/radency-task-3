b133b4fa700eebbe3c8eba2a3c3306697e3317ab
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2022-04-15 17:55:36 +0300

    update

--- helpers/createUrl.ts
+++ helpers/createUrl.ts
@@ -5,13 +5,13 @@ type UrlFn = (urlHost: string, elemPath: string ) => object;
 const createUrl:UrlFn = (urlHost, elemDbPath) => {
     const pathArr = elemDbPath.split(path.sep);
     const iconName = pathArr[pathArr.length - 1];
-    // const urlPath = path.join(...pathArr.filter((el, i, arr) => i !== arr.length - 1));
+    const urlPath = path.join(...pathArr.filter((el, i, arr) => i!=0 && i !== arr.length - 1));
 
     const urlParams = {
         protocol: 'http:',
         host: urlHost,
-        path: iconName
-        // iconName: iconName
+        path: urlPath,
+        iconName: iconName
     };
 
     const urlPrepack = Object.values(urlParams).join('/');
